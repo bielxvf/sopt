ae32cfadaea9b19354e5b0b7db5d7d9fdaf76a0f
Biel Sala <bielsalamimo@gmail.com>
2024-06-16 18:15:34 +0200

    First commit

---
+++ sopt.h
@@ -0,0 +1,15 @@
+#ifndef SOPT_H
+#define SOPT_H
+
+typedef struct {
+    const char *s;
+    const char *l;
+    const char *vals;
+    const char *usage;
+} Option;
+
+#endif // SOPT_H
+
+#ifdef SOPT_IMPLEMENTATION
+
+#endif // SOPT_IMPLEMENTATION
